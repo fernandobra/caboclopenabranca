1daf28753841bd755ad0c11fdefca7e3a0d1dee0
ferna <fernandost1974@gmail.com>
2025-04-24 15:16:55 -0300

    update data format json

--- src/main/java/com/umbandanet/caboclopenabranca/config/JacksonConfig.java
+++ src/main/java/com/umbandanet/caboclopenabranca/config/JacksonConfig.java
@@ -1,7 +1,7 @@
 package com.umbandanet.caboclopenabranca.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +11,9 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        objectMapper.setDateFormat(new java.text.SimpleDateFormat("yyyy/MM/dd"));
-        return objectMapper;
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Jdk8Module()); // Suporte para Optional
+        mapper.registerModule(new JavaTimeModule()); // Suporte para tipos de data/hora
+        return mapper;
     }
 }
