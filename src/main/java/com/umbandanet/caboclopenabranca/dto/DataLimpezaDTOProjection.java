63447960998a75cfdd694f8078ae5b3541ae535c
ferna <fernandost1974@gmail.com>
2025-04-26 15:08:20 -0300

    inclusão do jsonFormat na classe datalipeza

--- src/main/java/com/umbandanet/caboclopenabranca/dto/DataLimpezaDTOProjection.java
+++ src/main/java/com/umbandanet/caboclopenabranca/dto/DataLimpezaDTOProjection.java
@@ -1,9 +1,12 @@
 package com.umbandanet.caboclopenabranca.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public interface DataLimpezaDTOProjection {
     Long getId();
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     Date getData_limpeza();
     String getNome();
     String getGrupo();
--- src/main/java/com/umbandanet/caboclopenabranca/repository/DataLimpezaRepository.java
+++ src/main/java/com/umbandanet/caboclopenabranca/repository/DataLimpezaRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @Repository
 public interface DataLimpezaRepository extends JpaRepository<DataLimpeza, Long> {
-    @Query(value = "SELECT d.id, d.data_limpeza, p.nome, gl.grupo " +
+    @Query(value = "SELECT d.id, d.data_limpeza , p.nome, gl.grupo " +
             "FROM data_limpeza d " +
             "JOIN grupo_limpeza gl ON d.grupo_limpeza_id = gl.id " +
             "JOIN grupo_pessoa gp ON gl.id = gp.grupo_limpeza_id " +
